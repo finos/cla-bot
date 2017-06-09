db3cffa3800157adc388c29ac37cdb5495799f0f
Colin Eberhardt <colin.eberhardt@gmail.com>
2017-06-09 06:30:53 +0100

    Switched to airbnb linting rules

--- contributionVerifier.js
+++ contributionVerifier.js
@@ -1,31 +1,31 @@
 const requestp = require('./requestAsPromise');
-const {githubRequest, getFile} = require('./githubApi');
+const { githubRequest, getFile } = require('./githubApi');
 
 const clabotToken = process.env.GITHUB_ACCESS_TOKEN;
 
-const contributorArrayVerifier = (contributors) =>
-  (committers) =>
+const contributorArrayVerifier = contributors =>
+  committers =>
     Promise.resolve(committers.filter(c => contributors.indexOf(c) === -1));
 
-const configFileFromGithubUrlVerifier = (contributorListGithubUrl) =>
-  (committers) =>
+const configFileFromGithubUrlVerifier = contributorListGithubUrl =>
+  committers =>
     githubRequest({
       url: contributorListGithubUrl,
       method: 'GET'
     }, clabotToken)
-    .then((body) => githubRequest(getFile(body), clabotToken))
-    .then((contributors) => contributorArrayVerifier(contributors)(committers));
+    .then(body => githubRequest(getFile(body), clabotToken))
+    .then(contributors => contributorArrayVerifier(contributors)(committers));
 
-const configFileFromUrlVerifier = (contributorListUrl) =>
-  (committers) =>
+const configFileFromUrlVerifier = contributorListUrl =>
+  committers =>
     requestp({
       url: contributorListUrl,
       json: true
     })
-    .then((contributors) => contributorArrayVerifier(contributors)(committers));
+    .then(contributors => contributorArrayVerifier(contributors)(committers));
 
-const webhookVerifier = (webhookUrl) =>
-  (committers) =>
+const webhookVerifier = webhookUrl =>
+  committers =>
     Promise.all(committers.map(username =>
       requestp({
         url: webhookUrl,
@@ -33,7 +33,7 @@ const webhookVerifier = (webhookUrl) =>
           checkContributor: username
         }
       })
-      .then((response) => ({
+      .then(response => ({
         username,
         isContributor: response.isContributor
       }))
@@ -54,4 +54,5 @@ module.exports = (config) => {
   } else if (config.contributorWebhook) {
     return webhookVerifier(config.contributorWebhook);
   }
+  throw new Error('A mechanism for verifying contributors has not been specified');
 };

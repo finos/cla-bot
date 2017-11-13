4e86a787a1b415a6621f0052cfa3e966525443cf
Colin Eberhardt <colin.eberhardt@gmail.com>
2017-11-13 11:28:34 +0000

    logs now read from dynamodb

--- cla-bot/dev/index.js
+++ cla-bot/dev/index.js
@@ -1,8 +1,9 @@
-const fs = require('fs');
 const path = require('path');
-const lambda = require('../index');
+const fs = require('fs');
 
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+const lambda = require('../index');
 
 const event = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'event.json')));
 

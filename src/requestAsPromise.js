5aac0fa09d229d9e8e6c2df8be71186bb09fbb84
Colin E <colin.eberhardt@gmail.com>
2020-04-27 09:54:59 +0100

    fix: remove extraneous details from logging

--- src/requestAsPromise.js
+++ src/requestAsPromise.js
@@ -3,26 +3,27 @@ const logger = require("./logger");
 
 module.exports = options =>
   new Promise((resolve, reject) => {
-    logger.info(`API Request ${options.url}`, options);
+    const logUrl = options.url.split("?")[0];
+    logger.info(`API Request ${logUrl}`, options);
     request(options, (error, response, body) => {
       if (error) {
-        logger.debug(`API Response ${options.url}`, { error });
+        logger.debug(`API Response ${logUrl}`, { error });
         reject(error.toString());
       } else if (
         response &&
         response.statusCode &&
         !response.statusCode.toString().startsWith("2")
       ) {
-        logger.debug(`API Response ${options.url}`, {
+        logger.debug(`API Response ${logUrl}`, {
           statusCode: response.statusCode
         });
         reject(
           new Error(
-            `API request ${options.url} failed with status ${response.statusCode}`
+            `API request ${logUrl} failed with status ${response.statusCode}`
           )
         );
       } else {
-        logger.debug(`API Response ${options.url}`, body);
+        logger.debug(`API Response ${logUrl}`, body);
         resolve(body);
       }
     });
